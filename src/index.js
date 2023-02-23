eae70b4ecb08bcdb0f8a9cfa240c81a7b5479195
Javohirshakh <rajabov4you@gmail.com>
2023-02-23 15:28:00 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import '../index.css'
+import ICON from '../assets/javascript.png'
+
+const jsImageHTML = document.createElement('img')
+jsImageHTML.src = ICON
+jsImageHTML.className = 'js-image'
+
+document.body.append(jsImageHTML)
+const addd = document.querySelector
